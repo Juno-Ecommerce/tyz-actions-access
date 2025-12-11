bb56c1fdf9400221b0b0d3f1cdb88f837dcc6cb5
Sam Clarke <alruban@protonmail.com>
2025-12-11 17:28:23 +0000

    revert

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,6 +1,16 @@
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: PrismaClient;
+}
 
-export { prisma };
+if (process.env.NODE_ENV !== "production") {
+  if (!global.prismaGlobal) {
+    global.prismaGlobal = new PrismaClient();
+  }
+}
+
+const prisma = global.prismaGlobal ?? new PrismaClient();
+
+export default prisma;
